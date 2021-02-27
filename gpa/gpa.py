cebdebe2345625a00557c2cb769bf54ee1eefbbf
André Ricardo Fazanaro Martinon <martinon@gmail.com>
2021-02-27 14:15:56 -0300

    Implementation of the symmetrical magnitude coefficient (symmetric G2).

--- gpa/gpa.py
+++ gpa/gpa.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from typing import List
+
 
 class GPA:
 
     def __init__(self, lattice: np.ndarray, modulus_tolerance: float = 0,
                  phases_tolerance: float = 0):
         self.n = lattice.size
+
         self.shape = lattice.shape
         self.lattice = lattice
         self.modulus_tolerance = modulus_tolerance
@@ -17,57 +20,85 @@ class GPA:
         self.phases = np.arctan2(self.v, self.u)
 
         self._asymmetric_lattice()
+        self._symmetric_lattice()
 
-    def _asymmetric_lattice(self):
+    def _check_modulus(self, nan_value: int = 0):
         half_size = int(self.n / 2)
         odd = int(self.n % 2)
 
         modulus = np.ravel(self.modulus)
-        modulus_first_half = modulus[0:half_size+odd]
+        modulus_first_half = modulus[0:half_size + odd]
         modulus_second_half = np.flip(modulus[half_size:])
         modulus = np.abs(modulus_first_half - modulus_second_half)
         modulus[modulus <= self.modulus_tolerance] = 0
-        modulus[np.isnan(modulus)] = 0
+        modulus[np.isnan(modulus)] = nan_value
+        modulus = np.concatenate((modulus, np.flip(modulus[:half_size])))
+        return modulus.reshape(self.shape)
+
+    def _check_phases(self, nan_value: int = 0):
+        half_size = int(self.n / 2)
+        odd = int(self.n % 2)
 
         phases = np.ravel(self.phases)
-        phases_first_half = phases[0:half_size+odd]
+        phases_first_half = phases[0:half_size + odd]
         phases_second_half = np.flip(phases[half_size:])
         phases = np.abs(phases_first_half) + np.abs(phases_second_half)
         phases[phases >= (np.pi - self.phases_tolerance)] = 0
-        phases[np.isnan(phases)] = 0
+        phases[np.isnan(phases)] = nan_value
+        phases = np.concatenate((phases, np.flip(phases[:half_size])))
+        return phases.reshape(self.shape)
 
-        asymmetry_matrix = modulus + phases
-        asymmetry_matrix = np.concatenate((
-            asymmetry_matrix, np.flip(asymmetry_matrix[:half_size])))
-        asymmetry_matrix = asymmetry_matrix.reshape(self.shape)
+    def _asymmetric_lattice(self):
+        modulus = self._check_modulus()
+        phases = self._check_phases()
 
-        asymmetry_matrix[asymmetry_matrix != 0] = 1.0
+        self.asymmetric_lattice = (modulus + phases).astype('int64')
+        self.asymmetric_lattice[self.asymmetric_lattice > 0] = 1
+        self.asymmetric_lattice[self.asymmetric_lattice < 0] = 0
 
-        self.va = np.sum(asymmetry_matrix)
+        self.asymmetric_va = np.sum(self.asymmetric_lattice)
 
-        self.asymmetric_u = self.u * asymmetry_matrix
-        self.asymmetric_u[self.asymmetric_u == -0.0] = 0
+    def _symmetric_lattice(self):
+        modulus = self._check_modulus(nan_value=-1)
+        phases = self._check_phases(nan_value=-1)
 
-        self.asymmetric_v = self.v * asymmetry_matrix
-        self.asymmetric_v[self.asymmetric_v == -0.0] = 0
+        self.symmetric_lattice = (modulus + phases).astype('int64')
+        self.symmetric_lattice[self.symmetric_lattice != 0] = 1
+        self.symmetric_lattice = 1 - self.symmetric_lattice
 
-        self.asymmetric_modulus = np.sqrt(
-            self.asymmetric_u ** 2 + self.asymmetric_v ** 2)
-        self.asymmetric_phases = np.arctan2(
-            self.asymmetric_v, self.asymmetric_u)
+        self.symmetric_va = np.sum(self.symmetric_lattice)
 
-    def _asymmetrical_magnitude_coefficient(self):
-        modulus_of_sum = np.sqrt((np.sum(self.asymmetric_u))**2 +
-                                 (np.sum(self.asymmetric_v))**2)
-        sum_of_modulus = np.sum(self.asymmetric_modulus)
+    def _asymmetrical_magnitude_coefficient(self) -> float:
+        asymmetric_u = self.u * self.asymmetric_lattice
+        asymmetric_v = self.v * self.asymmetric_lattice
+        asymmetric_modulus = np.sqrt(asymmetric_u ** 2 + asymmetric_v ** 2)
+
+        modulus_of_sum = np.sqrt((np.sum(asymmetric_u))**2 +
+                                 (np.sum(asymmetric_v))**2)
+        sum_of_modulus = np.sum(asymmetric_modulus)
 
         with np.errstate(divide='ignore', invalid='ignore'):
             confluence = np.nan_to_num(modulus_of_sum/sum_of_modulus)
 
-        return self.va / self.n * (2 - confluence)
+        return self.asymmetric_va / self.n * (2 - confluence)
 
-    def evaluate(self):
-        return self._asymmetrical_magnitude_coefficient()
+    def _symmetrical_magnitude_coefficient(self) -> float:
+        symmetric_u = self.u * self.symmetric_lattice
+        symmetric_v = self.v * self.symmetric_lattice
+        symmetric_modulus = np.sqrt(symmetric_u ** 2 + symmetric_v ** 2)
+
+        modulus_of_sum = np.sqrt((np.sum(symmetric_u))**2 +
+                                 (np.sum(symmetric_v))**2)
+        sum_of_modulus = np.sum(symmetric_modulus)
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            confluence = np.nan_to_num(modulus_of_sum/sum_of_modulus)
+
+        return self.symmetric_va / self.n * (2 - confluence)
+
+    def evaluate(self) -> List[float]:
+        return [self._symmetrical_magnitude_coefficient(),
+                self._asymmetrical_magnitude_coefficient()]
 
     def plot(self):
         fig, ax = plt.subplots(figsize=(7, 7))
@@ -79,10 +110,21 @@ class GPA:
         plt.show()
 
     def plot_asymmetric(self):
-        x, y = np.meshgrid(np.arange(0, self.asymmetric_u.shape[1], 1),
-                           np.arange(0, self.asymmetric_v.shape[0], 1))
+        x, y = np.meshgrid(np.arange(0, self.u.shape[1], 1),
+                           np.arange(0, self.v.shape[0], 1))
 
         fig, ax = plt.subplots(figsize=(7, 7))
-        ax.quiver(x, y, self.asymmetric_u, self.asymmetric_v)
+        ax.quiver(x, y, self.u * self.asymmetric_lattice,
+                  self.v * self.asymmetric_lattice)
+
+        plt.show()
+
+    def plot_symmetric(self):
+        x, y = np.meshgrid(np.arange(0, self.u.shape[1], 1),
+                           np.arange(0, self.v.shape[0], 1))
+
+        fig, ax = plt.subplots(figsize=(7, 7))
+        ax.quiver(x, y, self.u * self.symmetric_lattice,
+                  self.v * self.symmetric_lattice)
 
         plt.show()
